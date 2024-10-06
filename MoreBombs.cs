021644914edb8d20728f82c0e3fab584808eb482
Andronomos <andronomos@proton.me>
2024-10-05 21:07:21 -0700

    Cleaned up the content registration

--- MoreBombs.cs
+++ MoreBombs.cs
@@ -9,22 +9,16 @@ public class MoreBombs : Mod
 {
     public override void Load()
     {
-        RegisterBomb("Snow", ItemID.SnowBlock, TileID.SnowBlock, DustID.SnowBlock);
-        RegisterBomb("Ash", ItemID.AshBlock, TileID.Ash, DustID.Ash);
-        RegisterBomb("Cloud", ItemID.Cloud, TileID.Cloud, DustID.Cloud);
-        RegisterBomb("Mud", ItemID.MudBlock, TileID.Mud, DustID.Mud);
-        RegisterBomb("Sand", ItemID.SandBlock, TileID.Sand, DustID.Sand);
-        RegisterBomb("Stone", ItemID.StoneBlock, TileID.Stone, DustID.Stone);
-    }
-
-    private void RegisterBomb(string name, short itemId, ushort tileId, short dustId)
-    {
-        ModItem bomb = AddBomb(name, itemId, tileId, dustId, 25);
-        AddBomb($"Sticky{name}", bomb.Type, tileId, dustId, 1, true);
+        CreateBomb("Snow", ItemID.SnowBlock, TileID.SnowBlock, DustID.SnowBlock);
+        CreateBomb("Ash", ItemID.AshBlock, TileID.Ash, DustID.Ash);
+        CreateBomb("Cloud", ItemID.Cloud, TileID.Cloud, DustID.Cloud);
+        CreateBomb("Mud", ItemID.MudBlock, TileID.Mud, DustID.Mud);
+        CreateBomb("Sand", ItemID.SandBlock, TileID.Sand, DustID.Sand);
+        CreateBomb("Stone", ItemID.StoneBlock, TileID.Stone, DustID.Stone);
     }
 
     /// <summary>
-    /// 
+    /// Creates the item and projectile
     /// </summary>
     /// <param name="name"></param>
     /// <param name="itemId">The item used in the recipe</param>
@@ -32,15 +26,18 @@ public class MoreBombs : Mod
     /// <param name="dustId">The dust produces by the explosion</param>
     /// <param name="itemCount">The number of items used in the recipe</param>
     /// <param name="isSticky">If the bomb is sticky</param>
-    public ModItem AddBomb(string name, int itemId, ushort tileId, short dustId, int itemCount = 25, bool isSticky = false)
+    public void CreateBomb(string name, int itemId, ushort tileId, short dustId, int itemCount = 25)
     {
         string bombName = $"{name}Bomb";
+        string stickyBombName = $"Sticky{bombName}";
 
-        MoreBombsProjectile projectile = new(bombName, tileId, dustId, isSticky);
+        MoreBombsProjectile projectile = new(bombName, tileId, dustId, false);
         AddContent(projectile);
-        MoreBombsItem bombItem = new(bombName, itemId, itemCount, projectile, isSticky);
+        MoreBombsItem bombItem = new(bombName, itemId, itemCount, projectile, false);
         AddContent(bombItem);
 
-        return bombItem;
+        MoreBombsProjectile stickyProjectile = new(stickyBombName, tileId, dustId, true);
+        AddContent(stickyProjectile);        
+        AddContent(new MoreBombsItem(stickyBombName, bombItem.Type, itemCount, stickyProjectile, true));
     }
 }
