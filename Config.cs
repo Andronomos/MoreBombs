495f5e046fd637f210b7a7cba5a2dec3aa596615
Andronomos <andronomos@proton.me>
2024-10-10 01:38:10 -0700

    Added configuration

---
+++ Config.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace MoreBombs;
+
+public class Config : ModConfig
+{
+    public override ConfigScope Mode => ConfigScope.ServerSide;
+
+    [Header("Bombs")]
+    [DefaultValue(9)]
+    public int ExplosionWidth;
+
+    [DefaultValue(9)]
+    public int ExplosionHeight;
+
+    [DefaultValue(true)]
+    public bool CircleExplosion;
+}
